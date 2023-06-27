30c732b51a4af6dcad1d547631355c3172e1802c
Kanan Abdullayev <94052935+kvnann@users.noreply.github.com>
2023-06-27 15:24:27 +0200

    Some changes for better deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    backend_host:"http://localhost:4000",
+    backend_host:"https://postman-backend-one.vercel.app",
     user_default:"https://t3.ftcdn.net/jpg/05/79/55/26/360_F_579552668_sZD51Sjmi89GhGqyF27pZcrqyi7cEYBH.jpg"
 }
 
